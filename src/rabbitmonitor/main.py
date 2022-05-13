f1dd5cb40dba5c42f8b77550415f928206211366
Ziga <ziga.zajc007@gmail.com>
2022-05-13 12:42:09 +0200

    Performance improvement

--- src/rabbitmonitor/main.py
+++ src/rabbitmonitor/main.py
@@ -1,35 +1,84 @@
-from ast import arg
-from xmlrpc.client import Boolean
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_httpauth import HTTPBasicAuth
+from quart import Quart, websocket
 from cpuinfo import get_cpu_info
 import psutil
 import platform
 import argparse
+import asyncio
+from threading import Thread
+from time import sleep
 
-app = Flask(__name__)
-api = Api(app)
-auth = HTTPBasicAuth()
+app = Quart(__name__)
 
 user = 'rabbit'
 passwd = ''
 
-class CPU(Resource):
-  @auth.login_required
-  def get(self):
-    return {
+@app.route("/cpu")
+async def cpu():
+  return data.get('cpu')
+
+@app.route("/memory")
+async def memory():
+  return data.get('memory')
+
+@app.route("/swap")
+async def swap():
+  return data.get('swap')
+
+@app.route("/storage")
+async def storage():
+  return data.get('storage')
+
+@app.route("/info")
+async def info():
+  return serverInfo
+
+@app.route("/stats")
+async def stats():
+  return data
+
+@app.websocket('/ws')
+async def ws():
+  while True:
+    data = await websocket.receive()
+    await websocket.send(f"echo {data}")
+
+def getInfo():
+  data = platform.freedesktop_os_release()
+  global serverInfo
+  serverInfo = {
+    'name': platform.node(),
+    'cpu': {
+      'name': get_cpu_info()['brand_raw'],
+      'cores': psutil.cpu_count(0),
+      'threads': psutil.cpu_count(1),
+    },
+    'system': {
+      'name': data['NAME'],
+      'id': data['ID'],
+      'pretty_name': data['PRETTY_NAME'],
+      'version': data['VERSION'],
+      'version_id': data['VERSION_ID'],
+      'logo': data['LOGO']
+    }
+  }
+
+def startDataFetcher():
+  while True:
+    fetchData()
+    sleep(5)
+
+def fetchData():
+  global data
+  memory = psutil.virtual_memory()
+  swap = psutil.swap_memory()
+  storage = psutil.disk_usage('/')
+  data = {
+    'cpu': {
       'load': psutil.getloadavg(),
       'frequency': psutil.cpu_freq(0),
       'frequencies': psutil.cpu_freq(1)
-    }, 200
-  pass  
-  
-class Memory(Resource):
-  @auth.login_required
-  def get(self):
-    memory = psutil.virtual_memory()
-    return {
+    },
+    'memory': {
       'total': memory[0],
       'available': memory[1],
       'percent': memory[2],
@@ -40,105 +89,20 @@ class Memory(Resource):
       'buffers': memory[7],
       'cached': memory[8],
       'shared': memory[9]
-    }, 200
-  pass
-
-class Swap(Resource):
-  @auth.login_required
-  def get(self):
-    swap = psutil.swap_memory()
-    return {
+    },
+    'swap': {
       'total': swap[0],
       'used': swap[1],
       'free': swap[2],
       'percent': swap[3]
-    }, 200
-  pass
-
-class Storage(Resource):
-  @auth.login_required
-  def get(self):
-    storage = psutil.disk_usage('/')
-    return {
+    },
+    'storage': {
       'total': storage[0],
       'used': storage[1],
       'free': storage[2],
       'percent': storage[3]
-    }, 200
-  pass
-
-class Info(Resource):
-  @auth.login_required
-  def get(self):
-    data = platform.freedesktop_os_release()
-    return {
-      'name': platform.node(),
-      'cpu': {
-        'name': get_cpu_info()['brand_raw'],
-        'cores': psutil.cpu_count(0),
-        'threads': psutil.cpu_count(1),
-      },
-      'system': {
-        'name': data['NAME'],
-        'id': data['ID'],
-        'pretty_name': data['PRETTY_NAME'],
-        'version': data['VERSION'],
-        'version_id': data['VERSION_ID'],
-        'logo': data['LOGO']
-      }
-    }, 200
-  pass
-
-class Stats(Resource):
-  @auth.login_required
-  def get(self):
-    memory = psutil.virtual_memory()
-    swap = psutil.swap_memory()
-    storage = psutil.disk_usage('/')
-    return {
-      'cpu': {
-        'load': psutil.getloadavg(),
-        'frequency': psutil.cpu_freq(0),
-        'frequencies': psutil.cpu_freq(1)
-      },
-      'memory': {
-        'total': memory[0],
-        'available': memory[1],
-        'percent': memory[2],
-        'used': memory[3],
-        'free': memory[4],
-        'active': memory[5],
-        'inactive': memory[6],
-        'buffers': memory[7],
-        'cached': memory[8],
-        'shared': memory[9]
-      },
-      "swap": {
-        'total': swap[0],
-        'used': swap[1],
-        'free': swap[2],
-        'percent': swap[3]
-      },
-      "storage": {
-        'total': storage[0],
-        'used': storage[1],
-        'free': storage[2],
-        'percent': storage[3]
-      }
-    }, 200
-  pass
-
-@auth.verify_password
-def verify(username, password):
-  if(username == user and password == passwd):
-    return username
-
-api.add_resource(CPU, '/cpu')
-api.add_resource(Memory, '/memory')
-api.add_resource(Swap, '/swap')
-api.add_resource(Storage, '/storage')
-api.add_resource(Info, '/info')
-api.add_resource(Stats, '/stats')
+    }
+  }
 
 if __name__ == '__main__':
 
@@ -153,4 +117,7 @@ if __name__ == '__main__':
   user = args.username
   passwd = args.password
 
-  app.run(args.host, args.port, args.debug, threaded=True)
+  getInfo()
+  Thread(target=startDataFetcher).start()
+
+  app.run(args.host, args.port, args.debug)
