f43f1271531ea1dc5068f16eafa5583c9a23723e
Ziga <ziga.zajc007@gmail.com>
2022-05-14 19:11:05 +0200

    Added support for Open Metrics

--- src/rabbitmonitor/main.py
+++ src/rabbitmonitor/main.py
@@ -19,9 +19,7 @@ data = {
 }
 
 networkSpeeds = {}
-
-user = 'rabbit'
-passwd = ''
+openMetrics = ""
 
 @app.route("/cpu")
 async def cpu():
@@ -54,6 +52,10 @@ async def system():
 @app.route("/stats")
 async def stats():
   return data
+
+@app.route("/metrics")
+async def metrics():
+  return openMetrics
 
 @app.websocket('/ws')
 async def ws():
@@ -131,6 +133,8 @@ def fetchData():
   data['sensors']['battery'] = battery
 
   data['system']['users'] = users
+
+  createMetrics()
 
 def slowFetchData():
   global data
@@ -311,19 +315,44 @@ def formatUsers(users):
 def calculateSpeed(old, new, time):
   return ((new - old) / time) * 8
 
+def createMetrics():
+  global openMetrics
+  metrics = ""
+  # Memory
+  metrics += createMetric('gauge', 'memory_total', 'Total memory in bytes', data['memory']['total'])
+  metrics += createMetric('gauge', 'memory_available', 'Available memory in bytes', data['memory']['available'])
+  metrics += createMetric('gauge', 'memory_percent', 'Used memory in percent', data['memory']['percent'])
+  metrics += createMetric('gauge', 'memory_used', 'Used memory in bytes', data['memory']['used'])
+  metrics += createMetric('gauge', 'memory_free', 'Free memory in bytes', data['memory']['free'])
+  metrics += createMetric('gauge', 'memory_active', 'Active memory in bytes', data['memory']['active'])
+  metrics += createMetric('gauge', 'memory_inactive', 'Inactive memory in bytes', data['memory']['inactive'])
+  metrics += createMetric('gauge', 'memory_buffers', 'Buffers memory in bytes', data['memory']['buffers'])
+  metrics += createMetric('gauge', 'memory_cached', 'Cached memory in bytes', data['memory']['cached'])
+  metrics += createMetric('gauge', 'memory_shared', 'Shared memory in bytes', data['memory']['shared'])
+  # Swap
+  metrics += createMetric('gauge', 'swap_total', 'Total swap storage in bytes', data['swap']['total'])
+  metrics += createMetric('gauge', 'swap_used', 'Used swap storage in bytes', data['swap']['used'])
+  metrics += createMetric('gauge', 'swap_free', 'Free swap storage in bytes', data['swap']['free'])
+  metrics += createMetric('gauge', 'swap_percent', 'Used swap storage in percent', data['swap']['percent'])
+  # Storage
+  metrics += createMetric('gauge', 'storage_total', 'Total storage in bytes', data['storage']['total'])
+  metrics += createMetric('gauge', 'storage_used', 'Used storage in bytes', data['storage']['used'])
+  metrics += createMetric('gauge', 'storage_free', 'Free storage in bytes', data['storage']['free'])
+  metrics += createMetric('gauge', 'storage_percent', 'Used storage in percent', data['storage']['percent'])
+  openMetrics = metrics
+
+def createMetric(type, name, description, value):
+  return "# HELP rabbit_%s %s\n# TYPE rabbit_%s %s\nrabbit_%s %s\n" % (name, description, name, type, name, value)
+
 if __name__ == '__main__':
 
   parser = argparse.ArgumentParser()
   parser.add_argument("--host", help="bind the server to specific host (default: 0.0.0.0)", type=str, default='0.0.0.0')
   parser.add_argument("--port", help="bind the server to specific port (default: 8088)", type=int, default=8088)
-  parser.add_argument("--refresh", help="data will be fetched every x seconds (default: 1)", type=int, default=1)
-  parser.add_argument("--username", help="protect api endpoints with an username (default: rabbit)", type=str, default='rabbit')
-  parser.add_argument("--password", help="protect api endpoints with a password (default: none)", type=str, default='')
+  parser.add_argument("--refresh", help="data will be fetched every x seconds (default: 5)", type=int, default=5)
   parser.add_argument("--debug", help="enable debug mode (default: False)", action='store_true', default=False)
   args = parser.parse_args()
 
-  user = args.username
-  passwd = args.password
   refresh = args.refresh
 
   Thread(target=startDataFetcher).start()
